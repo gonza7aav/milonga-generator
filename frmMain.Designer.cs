6304bbf170391b7aba459f2bc0f2ef89db7e6431
gonza7aav <gonza.a.aguirre@gmail.com>
2021-02-09 15:34:04 -0300

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace serendipia_playlist_generator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.txtGenreSongQuatity = new System.Windows.Forms.TextBox();
+            this.chkMakeInterval = new System.Windows.Forms.CheckBox();
+            this.btnSaveConfiguration = new System.Windows.Forms.Button();
+            this.chkUseSameInterval = new System.Windows.Forms.CheckBox();
+            this.cmbGenres = new System.Windows.Forms.ComboBox();
+            this.btnGenreRemove = new System.Windows.Forms.Button();
+            this.dlgFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblGenreSongQuatity = new System.Windows.Forms.Label();
+            this.lblGenres = new System.Windows.Forms.Label();
+            this.btnIntervalsFolder = new System.Windows.Forms.Button();
+            this.txtIntervalsFolder = new System.Windows.Forms.TextBox();
+            this.lblIntervals = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(247, 219);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // txtGenreSongQuatity
+            // 
+            this.txtGenreSongQuatity.Location = new System.Drawing.Point(121, 71);
+            this.txtGenreSongQuatity.Name = "txtGenreSongQuatity";
+            this.txtGenreSongQuatity.Size = new System.Drawing.Size(50, 20);
+            this.txtGenreSongQuatity.TabIndex = 4;
+            this.txtGenreSongQuatity.Text = "3";
+            this.txtGenreSongQuatity.TextChanged += new System.EventHandler(this.txtGenreSongQuatity_TextChanged);
+            this.txtGenreSongQuatity.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtGenreSongQuatity_KeyPress);
+            // 
+            // chkMakeInterval
+            // 
+            this.chkMakeInterval.AutoSize = true;
+            this.chkMakeInterval.Checked = true;
+            this.chkMakeInterval.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkMakeInterval.Location = new System.Drawing.Point(12, 171);
+            this.chkMakeInterval.Name = "chkMakeInterval";
+            this.chkMakeInterval.Size = new System.Drawing.Size(111, 17);
+            this.chkMakeInterval.TabIndex = 5;
+            this.chkMakeInterval.Text = "Agregar intervalos";
+            this.chkMakeInterval.UseVisualStyleBackColor = true;
+            this.chkMakeInterval.CheckedChanged += new System.EventHandler(this.chkMakeInterval_CheckedChanged);
+            // 
+            // btnSaveConfiguration
+            // 
+            this.btnSaveConfiguration.Location = new System.Drawing.Point(12, 219);
+            this.btnSaveConfiguration.Name = "btnSaveConfiguration";
+            this.btnSaveConfiguration.Size = new System.Drawing.Size(159, 23);
+            this.btnSaveConfiguration.TabIndex = 6;
+            this.btnSaveConfiguration.Text = "Guardar Configuración";
+            this.btnSaveConfiguration.UseVisualStyleBackColor = true;
+            this.btnSaveConfiguration.Click += new System.EventHandler(this.btnSaveConfiguration_Click);
+            // 
+            // chkUseSameInterval
+            // 
+            this.chkUseSameInterval.AutoSize = true;
+            this.chkUseSameInterval.Location = new System.Drawing.Point(188, 171);
+            this.chkUseSameInterval.Name = "chkUseSameInterval";
+            this.chkUseSameInterval.Size = new System.Drawing.Size(134, 17);
+            this.chkUseSameInterval.TabIndex = 7;
+            this.chkUseSameInterval.Text = "Usar el mismo intervalo";
+            this.chkUseSameInterval.UseVisualStyleBackColor = true;
+            this.chkUseSameInterval.CheckedChanged += new System.EventHandler(this.chkUseSameInterval_CheckedChanged);
+            // 
+            // cmbGenres
+            // 
+            this.cmbGenres.FormattingEnabled = true;
+            this.cmbGenres.Items.AddRange(new object[] {
+            "Agregar nuevo..."});
+            this.cmbGenres.Location = new System.Drawing.Point(12, 25);
+            this.cmbGenres.Name = "cmbGenres";
+            this.cmbGenres.Size = new System.Drawing.Size(229, 21);
+            this.cmbGenres.TabIndex = 8;
+            this.cmbGenres.SelectedIndexChanged += new System.EventHandler(this.cmbGenres_SelectedIndexChanged);
+            // 
+            // btnGenreRemove
+            // 
+            this.btnGenreRemove.Location = new System.Drawing.Point(247, 25);
+            this.btnGenreRemove.Name = "btnGenreRemove";
+            this.btnGenreRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnGenreRemove.TabIndex = 9;
+            this.btnGenreRemove.Text = "Borrar";
+            this.btnGenreRemove.UseVisualStyleBackColor = true;
+            this.btnGenreRemove.Visible = false;
+            this.btnGenreRemove.Click += new System.EventHandler(this.btnGenreRemove_Click);
+            // 
+            // dlgFolder
+            // 
+            this.dlgFolder.Description = "Agregar nuevo";
+            // 
+            // lblGenreSongQuatity
+            // 
+            this.lblGenreSongQuatity.AutoSize = true;
+            this.lblGenreSongQuatity.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGenreSongQuatity.Location = new System.Drawing.Point(9, 74);
+            this.lblGenreSongQuatity.Name = "lblGenreSongQuatity";
+            this.lblGenreSongQuatity.Size = new System.Drawing.Size(106, 13);
+            this.lblGenreSongQuatity.TabIndex = 10;
+            this.lblGenreSongQuatity.Text = "Cantidad por carpeta";
+            // 
+            // lblGenres
+            // 
+            this.lblGenres.AutoSize = true;
+            this.lblGenres.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGenres.Location = new System.Drawing.Point(9, 9);
+            this.lblGenres.Name = "lblGenres";
+            this.lblGenres.Size = new System.Drawing.Size(49, 13);
+            this.lblGenres.TabIndex = 12;
+            this.lblGenres.Text = "Carpetas";
+            // 
+            // btnIntervalsFolder
+            // 
+            this.btnIntervalsFolder.Location = new System.Drawing.Point(247, 145);
+            this.btnIntervalsFolder.Name = "btnIntervalsFolder";
+            this.btnIntervalsFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnIntervalsFolder.TabIndex = 13;
+            this.btnIntervalsFolder.Text = "Buscar";
+            this.btnIntervalsFolder.UseVisualStyleBackColor = true;
+            this.btnIntervalsFolder.Click += new System.EventHandler(this.btnIntervalsFolder_Click);
+            // 
+            // txtIntervalsFolder
+            // 
+            this.txtIntervalsFolder.Enabled = false;
+            this.txtIntervalsFolder.Location = new System.Drawing.Point(12, 145);
+            this.txtIntervalsFolder.Name = "txtIntervalsFolder";
+            this.txtIntervalsFolder.Size = new System.Drawing.Size(229, 20);
+            this.txtIntervalsFolder.TabIndex = 14;
+            this.txtIntervalsFolder.TextChanged += new System.EventHandler(this.txtIntervalsFolder_TextChanged);
+            // 
+            // lblIntervals
+            // 
+            this.lblIntervals.AutoSize = true;
+            this.lblIntervals.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIntervals.Location = new System.Drawing.Point(9, 129);
+            this.lblIntervals.Name = "lblIntervals";
+            this.lblIntervals.Size = new System.Drawing.Size(134, 13);
+            this.lblIntervals.TabIndex = 15;
+            this.lblIntervals.Text = "Ubicación de los intervalos";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 261);
+            this.Controls.Add(this.lblIntervals);
+            this.Controls.Add(this.txtIntervalsFolder);
+            this.Controls.Add(this.btnIntervalsFolder);
+            this.Controls.Add(this.lblGenres);
+            this.Controls.Add(this.lblGenreSongQuatity);
+            this.Controls.Add(this.btnGenreRemove);
+            this.Controls.Add(this.cmbGenres);
+            this.Controls.Add(this.chkUseSameInterval);
+            this.Controls.Add(this.btnSaveConfiguration);
+            this.Controls.Add(this.chkMakeInterval);
+            this.Controls.Add(this.txtGenreSongQuatity);
+            this.Controls.Add(this.btnGenerar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.Text = "Serendipia Playlist Generator";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.TextBox txtGenreSongQuatity;
+        private System.Windows.Forms.CheckBox chkMakeInterval;
+        private System.Windows.Forms.Button btnSaveConfiguration;
+        private System.Windows.Forms.CheckBox chkUseSameInterval;
+        private System.Windows.Forms.ComboBox cmbGenres;
+        private System.Windows.Forms.Button btnGenreRemove;
+        private System.Windows.Forms.FolderBrowserDialog dlgFolder;
+        private System.Windows.Forms.Label lblGenreSongQuatity;
+        private System.Windows.Forms.Label lblGenres;
+        private System.Windows.Forms.Button btnIntervalsFolder;
+        private System.Windows.Forms.TextBox txtIntervalsFolder;
+        private System.Windows.Forms.Label lblIntervals;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace serendipia_playlist_generator
+{
+    public partial class frmMain : Form
+    {
+        List<Genre> genres;
+        int genreSongQuantity;
+        bool makeIntervals;
+        bool useSameInterval;
+        string intervalsFolder;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            genres = new List<Genre>();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // Load config
+            try
+            {
+                genreSongQuantity = Properties.Settings.Default.genreSongQuantity;
+                txtGenreSongQuatity.Text = genreSongQuantity.ToString();
+            }
+            catch
+            {
+                Console.WriteLine("genreSongQuantity configuration not found.");
+            }
+
+            try
+            {
+                makeIntervals = Properties.Settings.Default.makeIntervals;
+                chkMakeInterval.Checked = makeIntervals;
+            }
+            catch
+            {
+                Console.WriteLine("makeIntervals configuration not found.");
+            }
+
+            try
+            {
+                useSameInterval = Properties.Settings.Default.useSameInterval;
+                chkUseSameInterval.Checked = useSameInterval;
+            }
+            catch
+            {
+                Console.WriteLine("useSameInterval configuration not found.");
+            }
+
+            try
+            {
+                intervalsFolder = Properties.Settings.Default.intervalsFolder;
+                txtIntervalsFolder.Text = intervalsFolder;
+            }
+            catch
+            {
+                Console.WriteLine("intervalsFolder configuration not found.");
+            }
+
+            // Load genres
+            try
+            {
+                if (Properties.Settings.Default.genres != null)
+                {
+                    foreach (string genrePath in Properties.Settings.Default.genres)
+                    {
+                        genres.Add(new Genre(genrePath));
+                    }
+                    updateGenresComboBox();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("genres configuration not found.");
+            }
+        }
+
+        private void btnSaveConfiguration_Click(object sender, EventArgs e)
+        {
+            DialogResult confirmation = MessageBox.Show(
+                   "¿Desea guardar la configuración?",
+                   "Confirmación",
+                   MessageBoxButtons.YesNo,
+                   MessageBoxIcon.Question,
+                   MessageBoxDefaultButton.Button2);
+
+            if (confirmation == DialogResult.Yes)
+            {
+                Properties.Settings.Default.genreSongQuantity = genreSongQuantity;
+                Properties.Settings.Default.makeIntervals = makeIntervals;
+                Properties.Settings.Default.useSameInterval = useSameInterval;
+                Properties.Settings.Default.intervalsFolder = intervalsFolder;
+
+                ArrayList aux = new ArrayList();
+                foreach (Genre g in genres)
+                {
+                    aux.Add(g.path);
+                }
+                Properties.Settings.Default.genres = aux;
+
+                Properties.Settings.Default.Save();
+                Console.WriteLine("genres configuration saved.");
+            }
+        }
+
+        private void txtGenreSongQuatity_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtGenreSongQuatity.Text))
+            {
+                genreSongQuantity = int.Parse(txtGenreSongQuatity.Text);
+            }
+        }
+
+        private void chkMakeInterval_CheckedChanged(object sender, EventArgs e)
+        {
+            makeIntervals = chkMakeInterval.Checked;
+            btnIntervalsFolder.Enabled = chkMakeInterval.Checked;
+            chkUseSameInterval.Enabled = chkMakeInterval.Checked;
+        }
+
+        private void chkUseSameInterval_CheckedChanged(object sender, EventArgs e)
+        {
+            useSameInterval = chkUseSameInterval.Checked;
+        }
+
+        private void txtIntervalsFolder_TextChanged(object sender, EventArgs e)
+        {
+            intervalsFolder = txtIntervalsFolder.Text;
+        }
+
+        private void cmbGenres_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Add new one
+            if (cmbGenres.SelectedItem.ToString() == "Agregar nuevo...")
+            {
+                btnGenreRemove.Visible = false;
+
+                DialogResult result = dlgFolder.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    genres.Add(new Genre(dlgFolder.SelectedPath));
+                    updateGenresComboBox();
+                }
+            }
+            else
+            {
+                btnGenreRemove.Visible = true;
+            }
+        }
+
+        private void btnGenreRemove_Click(object sender, EventArgs e)
+        {
+            int index = genres.FindIndex(x => x.name == cmbGenres.SelectedItem.ToString());
+            genres.RemoveAt(index);
+            updateGenresComboBox();
+        }
+
+        private void btnIntervalsFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult result = dlgFolder.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                intervalsFolder = dlgFolder.SelectedPath;
+                txtIntervalsFolder.Text = intervalsFolder;
+            }
+        }
+
+        private void txtGenreSongQuatity_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // only numbers
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            if (!validate())
+            {
+                return;
+            }
+
+            DialogResult result = dlgFolder.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string path = dlgFolder.SelectedPath;
+                string content = "";
+
+                if (Directory.GetFiles(intervalsFolder).Length < genres.Count - 1)
+                {
+                    DialogResult confirmation = MessageBox.Show(
+                        "La carpeta seleccionada de intervalos no posee la cantidad de canciones necesarias.\n¿Desea que se repitan?",
+                        "Confirmación",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Warning,
+                        MessageBoxDefaultButton.Button2);
+                    if (confirmation != DialogResult.Yes)
+                    {
+                        return;
+                    }
+                }
+
+                List<string> intervalsSorted = getFilesSorted(intervalsFolder);
+                List<Genre> genresSorted = getGenreSorted();
+                string aux;
+
+                foreach (Genre g in genresSorted)
+                {
+                    List<string> filesSorted = getFilesSorted(g.path);
+
+                    content += (!string.IsNullOrWhiteSpace(content) ? "\n\n" : "") + "# Genre " + g.name;
+                    Console.WriteLine("# Genre " + g.name);
+
+                    for (int i = 0; i < genreSongQuantity; i++)
+                    {
+                        if (filesSorted.Count == 0)
+                        {
+                            filesSorted = getFilesSorted(g.path);
+                        }
+
+                        aux = filesSorted.ElementAt(0);
+                        content += "\n" + aux;
+                        Console.WriteLine(aux);
+                        filesSorted.RemoveAt(0);
+                    }
+
+                    if (makeIntervals && g != genresSorted.Last())
+                    {
+                        if (intervalsSorted.Count == 0)
+                        {
+                            intervalsSorted = getFilesSorted(intervalsFolder);
+                        }
+
+                        aux = intervalsSorted.ElementAt(0);
+                        content += "\n\n# Interval\n" + aux;
+                        Console.WriteLine(aux);
+                        if (!useSameInterval)
+                        {
+                            intervalsSorted.RemoveAt(0);
+                        }
+                    }
+                }
+
+                saveFile(path, content);
+            }
+
+        }
+
+        private void updateGenresComboBox()
+        {
+            cmbGenres.Items.Clear();
+            cmbGenres.Items.Add("Agregar nuevo...");
+            foreach (Genre g in genres)
+            {
+                cmbGenres.Items.Add(g.name);
+            }
+            cmbGenres.Text = "";
+
+            Console.WriteLine("Genres update: " + genres.Count());
+        }
+
+        private bool validate()
+        {
+            bool canGenerate = true;
+
+            canGenerate &= genres.Count >= 2;
+            canGenerate &= genreSongQuantity >= 1;
+            canGenerate &= !makeIntervals || !string.IsNullOrEmpty(txtIntervalsFolder.Text);
+
+            Console.WriteLine("validate: " + canGenerate.ToString());
+            return canGenerate;
+        }
+
+        private void saveFile(string _where, string _content)
+        {
+            string playlistName = "Playlist_" + DateTime.Now.Year.ToString() + "_"
+                + DateTime.Now.Month.ToString() + "_" + DateTime.Now.Day.ToString() + "_"
+                + DateTime.Now.Hour.ToString() + "_" + DateTime.Now.Minute.ToString() + "_"
+                + DateTime.Now.Second.ToString() + ".m3u";
+            string path = _where + "/" + playlistName;
+
+            File.WriteAllText(path, _content);
+        }
+
+        private List<string> getFilesSorted(string _path)
+        {
+            string[] files = Directory.GetFiles(_path);
+            List<string> list = new List<string>(files);
+            var random = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                string aux = list[k];
+                list[k] = list[n];
+                list[n] = aux;
+            }
+            return list;
+        }
+
+        private List<Genre> getGenreSorted()
+        {
+            List<Genre> list = new List<Genre>(genres);
+            var random = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                Genre aux = list[k];
+                list[k] = list[n];
+                list[n] = aux;
+            }
+            return list;
+        }
+    }
+}
